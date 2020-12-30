364454e8cb96e0b6dcb755b32abfd9b7bc340246
Vinesh <vinesh.nair31@gmail.com>
2020-12-30 22:02:09 +0530

    Added Dashboard project js

---
+++ Dashboard__project.js
@@ -0,0 +1,39 @@
+let dropdownBtn1 = document.querySelector('.Projects__conatiner__dropdwn__btn1');
+let menuContent1 = document.querySelector('.Projects__conatiner__dropdwn__data1');
+dropdownBtn1.addEventListener('click',()=>{
+   if(menuContent1.style.display===""){
+      menuContent1.style.display="block";
+   } else {
+      menuContent1.style.display="";
+   }
+})
+
+let dropdownBtn2 = document.querySelector('.Projects__conatiner__dropdwn__btn2');
+let menuContent2 = document.querySelector('.Projects__conatiner__dropdwn__data2');
+dropdownBtn2.addEventListener('click',()=>{
+   if(menuContent2.style.display===""){
+      menuContent2.style.display="block";
+   } else {
+      menuContent2.style.display="";
+   }
+})
+
+let dropdownBtn3 = document.querySelector('.Projects__conatiner__dropdwn__btn3');
+let menuContent3 = document.querySelector('.Projects__conatiner__dropdwn__data3');
+dropdownBtn3.addEventListener('click',()=>{
+   if(menuContent3.style.display===""){
+      menuContent3.style.display="block";
+   } else {
+      menuContent3.style.display="";
+   }
+})
+
+let dropdownBtn4 = document.querySelector('.Projects__conatiner__dropdwn__btn4');
+let menuContent4 = document.querySelector('.Projects__conatiner__dropdwn__data4');
+dropdownBtn4.addEventListener('click',()=>{
+   if(menuContent4.style.display===""){
+      menuContent4.style.display="block";
+   } else {
+      menuContent4.style.display="";
+   }
+})
