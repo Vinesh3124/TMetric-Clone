846b9253c26242055971104a2b7af983ba96134f
Vinesh <vinesh.nair31@gmail.com>
2021-01-01 12:26:07 +0530

    Updated the fonts of all pages fixed some alignment issues linked all the pages

--- Dashboard__project.js
+++ Dashboard__project.js
@@ -15,38 +15,61 @@ function myFunctiondropdown() {
  }
  
  // Close the dropdown if the user clicks outside of it
- window.onclick = function(e) {
-   if (!e.target.matches('.dropbtn')) {
-   var myDropdown = document.getElementById("myDropdown");
-     if (myDropdown.classList.contains('show')) {
-       myDropdown.classList.remove('show');
-     }
-   }
- }
+//  window.onclick = function(e) {
+//    if (!e.target.matches('.dropbtn')) {
+//    var myDropdown = document.getElementById("myDropdown");
+//      if (myDropdown.classList.contains('show')) {
+//        myDropdown.classList.remove('show');
+//      }
+//    }
+//  }
  
- window.onclick = function(e) {
-   if (!e.target.matches('.dropbtn1')) {
-   var myDropdown1 = document.getElementById("myDropdown1");
-     if (myDropdown1.classList.contains('show')) {
-       myDropdown1.classList.remove('show');
-     }
-   }
- }
+//  window.onclick = function(e) {
+//    if (!e.target.matches('.dropbtn1')) {
+//    var myDropdown1 = document.getElementById("myDropdown1");
+//      if (myDropdown1.classList.contains('show')) {
+//        myDropdown1.classList.remove('show');
+//      }
+//    }
+//  }
  
- window.onclick = function(e) {
-   if (!e.target.matches('.dropbtn2')) {
-   var myDropdown2 = document.getElementById("myDropdown2");
-     if (myDropdown2.classList.contains('show')) {
-       myDropdown2.classList.remove('show');
-     }
-   }
- }
+//  window.onclick = function(e) {
+//    if (!e.target.matches('.dropbtn2')) {
+//    var myDropdown2 = document.getElementById("myDropdown2");
+//      if (myDropdown2.classList.contains('show')) {
+//        myDropdown2.classList.remove('show');
+//      }
+//    }
+//  }
  
- window.onclick = function(e) {
-   if (!e.target.matches('.dropbtn3')) {
-   var myDropdown3 = document.getElementById("myDropdown3");
-     if (myDropdown3.classList.contains('show')) {
-       myDropdown3.classList.remove('show');
-     }
-   }
- }
+//  window.onclick = function(e) {
+//    if (!e.target.matches('.dropbtn3')) {
+//    var myDropdown3 = document.getElementById("myDropdown3");
+//      if (myDropdown3.classList.contains('show')) {
+//        myDropdown3.classList.remove('show');
+//      }
+//    }
+//  }
+
+// -----------------------------New Project--------------------------------
+
+var sub = document.getElementById("submitNew")
+var can = document.getElementById("cancelNew")
+
+windows.onload = function submit(){
+  var pname = document.getElementById("newProjectName").value
+  var pcode = document.getElementById("newProjectid").value
+  var note = document.getElementById("newProjectNotes").value
+
+  console.log(pname)
+  localStorage.setItem("ProjectName", pname)
+
+  var getName = localStorage.getItem("ProjectName")
+  console.log(getName)
+  document.getElementById("projectList").innerHTML = getName
+  
+}
+
+
+sub.addEventListener("click", submit)
+// sub.addEventListener("click", cancel)
